bbee244083d5dc65c17490aa4856edf4b4206bbb
Ingibjörg Ösp Finnsdóttir <osp07@users.noreply.github.com>
2026-01-18 14:36:12 +0000

    nokkur comment

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 import { useState } from "react";
 import ColorType from "./props/page";
+// importum proppinu ColorType sem búið var til á annarri síðu
 
 export default function Home() {
 
   const [color, setColor] = useState('Red');
+  // color er breyta sem geymir gildið
+  // setColor er notað til að uppfæra gildið
+  // useState() er gildið sem við byrjum með áður en því hefur verið breytt
 
   function Red(){
+    // þegar kallað er á fallið verður color breytunni breytt
     setColor('Red');
   }
   function Orange(){
